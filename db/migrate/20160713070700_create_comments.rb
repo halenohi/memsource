dacc3559e70be9d7daab2bc1fbd900f66d171f2f
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-14 08:45:42 +0900

    user_id,cirlceを修正しました。

--- db/migrate/20160713070700_create_comments.rb
+++ db/migrate/20160713070700_create_comments.rb
@@ -1,10 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :circle
+      t.integer :user_id, null: false
+      t.integer :circle, null: false
       t.text :content
 
+      t.integer [:user_id, :cirlce]
       t.timestamps
     end
   end
