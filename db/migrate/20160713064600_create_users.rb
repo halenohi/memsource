354d11aed53d94342aadf44555b4ca4d908e4117
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-13 16:50:35 +0900

    put to columu null: false and made index

--- db/migrate/20160713064600_create_users.rb
+++ db/migrate/20160713064600_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password
-      t.string :email
+      t.string :name, null: false
+      t.string :password, null: false
+      t.string :email, null: false
+
+      # index生成
+      add_index :name, :password, :email
 
       t.timestamps
     end
