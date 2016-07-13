35d6fd989c5c8b160f8c928ae99f89f3f0d8d7a7
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-13 17:07:18 +0900

    put to columu null: false and made index

--- db/migrate/20160713065911_create_circles.rb
+++ db/migrate/20160713065911_create_circles.rb
@@ -1,9 +1,12 @@
 class CreateCircles < ActiveRecord::Migration[5.0]
   def change
     create_table :circles do |t|
-      t.string :name
-      t.string :description
+      t.string :name, null: false
+      t.string :description, null: false
       t.int :members_count
+
+      #index生成
+      add_index :name, :description
 
       t.timestamps
     end
