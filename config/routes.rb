4be2a958e6b26ceeaed852f46f8ea3c0bed1f0d1
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-14 20:19:21 +0900

    routesの修正をしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
   resources :users, only: [:new, :create, :destroy]
 
   resources :circles do
     resources :memberships, only: [:create] do
-      resource :acceptance, only: [:create]
+      resource :acceptance, only: [:create], module: :memberships
     end
-    resources :comments, only: [:edit, :new, :create]
+    resources :comments, only: [:edit, :new, :create, :update], module: :circles
   end
 
   resources :comments, only: [:destroy]
-
-  # comment
-  # resources :comments#, :only => [:index, :destroy, :create, :edit, :show]
-
-  # user
-  # devise_for :users, skip: [:sessions]
-  # get "/login" => "user_sessions#new", as: :new_user_session
-  # post "/login" => "user_sessions#create"
-  # get "/logout" => "user_sessions#destroy", as: :destroy_user_session
-
-  # circle
-  # resources :circles, :only => [:index, :show, :new, :create, :destroy, :edit, :update]
-  # get "/circles" => "circles/sessions#inedx", as: :index_circle_session
-  # get "/circles" => "circle/sessions#new", as: :new_circle_session
-  # post "/circles" => "circle/sessions#create"
-  # post "/circle" => "circle/id/membership#create"
-  # post "/circle" => "circle/id/membership/id#new", as: :new_memberships_session
-  # delete "/circle" => "circle/membership#destroy", as: :destroy_memberships_session
-
 end
