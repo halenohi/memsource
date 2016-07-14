40bbd039a96ab01c1c03b74358e7da210740cd91
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-14 12:36:15 +0900

    members_countを修正しました。

--- db/migrate/20160713065911_create_circles.rb
+++ db/migrate/20160713065911_create_circles.rb
@@ -3,11 +3,10 @@ class CreateCircles < ActiveRecord::Migration[5.0]
     create_table :circles do |t|
       t.string :name, null: false
       t.text :description
-      t.integer :members_count
+      t.integer :members_count, default: 0
 
       #index生成
       t.index [:name]
-      t.integer :members_count, default: 0
 
       t.timestamps
     end
