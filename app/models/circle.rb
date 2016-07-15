cae6187ba556554cd93f9f418fc1ecdc0f09d894
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-15 11:08:04 +0900

    define circle model

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,2 +1,5 @@
 class Circle < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :members_count, presence: true
 end
