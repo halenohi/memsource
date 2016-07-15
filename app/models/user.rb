c30a41c7b81ec329d1a1a56487a5dd14a22f4c86
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-15 10:52:58 +0900

    define user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, email: true
+  validates :password, presence: true, length: { minimum: 5 }
 end
