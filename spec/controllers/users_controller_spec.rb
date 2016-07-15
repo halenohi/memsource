ee51102124595a48242102776440bb82c5125d42
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-15 18:09:30 +0900

    addtion confirmation

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,21 +3,24 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #new" do
-    it "returns http success" do
+    xit "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    context 'succes' do
+
+    end
+
+    context 'fail' do
+      
     end
   end
 
   describe "GET #destroy" do
-    it "returns http success" do
+    xit "returns http success" do
       get :destroy
       expect(response).to have_http_status(:success)
     end
