e2e9eec1c11a4bf1bcbb00773927854abdadd974
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-07-15 12:21:25 +0900

    define user.circles association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 5 }
 
   has_many :memberships
+  has_many :circles, through: :memberships
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,15 @@ RSpec.describe User, type: :model do
     FactoryGirl.create(:user)
   end
 
+  let!(:circle) do
+    FactoryGirl.create(:circle)
+  end
+
   describe 'associations' do
     it 'should has many memberships' do
-      expect(user.memberships.create).to be_an_instance_of(Membership)
+      membership = user.memberships.create(circle: circle)
+      expect(membership).to be_an_instance_of(Membership)
+      expect(user.circles).to eq([circle])
     end
   end
 end
